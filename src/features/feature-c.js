3f60aeb15d3f2841201f4ae593b5cbe67f745c54
ahonn <ahonn95@outlook.com>
2022-07-04 13:20:22 +0800

    feat: add longtask

--- src/features/feature-c.js
+++ src/features/feature-c.js
@@ -1,3 +1,9 @@
+console.log('import feature-c');
+
 export default function featureC() {
+  for (let i = 0; i < 10 ** 4; i++) {
+    console.log(`run feature-c for ${i}`);
+  }
+
   console.log('run feature-c');
 }
--- src/features/feature-d.js
+++ src/features/feature-d.js
@@ -1,3 +1,5 @@
+console.log('import feature-d');
+
 export default function featureD() {
   console.log('run feature-d');
 }
